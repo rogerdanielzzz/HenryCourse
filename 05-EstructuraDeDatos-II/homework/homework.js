ae420db5f6fb005faaf4021d5579e2e57b88c541
Roger Perez <roger.perezcol@gmail.com>
2022-07-26 13:28:43 -0300

    Homework 6

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -1,19 +1,116 @@
 "use strict";
 
+const {
+  has
+} = require("markdown-it/lib/common/utils");
+
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+
+  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una 
+    lista de un solo nodo y de una lista vacía);
+
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser
+   un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el 
+   segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true.
+
   Ejemplo: 
   search(3) busca un nodo cuyo valor sea 3;
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
+class Node {
+  constructor(data, ) {
+    this.data = data;
+    this.next = null;
 
-function LinkedList() {}
+  }
+}
 
-function Node(value) {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.lenght = 0;
+  }
+  add(data) {
+    var node = new Node(data);
+    var actual = this.head;
+    if (!this.head) {
+      this.lenght++;
+      this.head = node;
+      return node
+    }
+
+    while (actual.next) {
+      actual = actual.next;
+    }
+
+    actual.next = node;
+    this.lenght++;
+   }
+  remove() {
+    if (!this.head) return false;
+
+  if (this.head.next === null) {
+    
+    let aux = this.head.data;
+    this.head = null;
+ 
+    return aux;
+  } else {
+    let current = this.head;
+    while (current.next.next !== null) {
+      current = current.next;
+    }
+    let aux = current.next.data; 
+    current.next = null;
+   
+    return aux;
+  }
+
+
+  }
+  search(x) {
+    if (!this.head) return null
+
+   let current = this.head
+
+    while (current){
+      if (typeof x == "function"){
+         if (x(current.data)=== current.data) return true        
+      } else if (x== current.data) return current.data;
+      else  current = current.next
+      }
+
+    return null
+
+
+
+ /*   let actual = this.head;
+    if (this.head == x) {
+      console.log(actual.data)
+      return actual.data
+    }
+    while (actual.next) {
+      actual = actual.next;
+      if (actual.data == x) {
+        if (typeof (actual.data) === "function") {
+          console.log("verdad")
+          return true
+        } else {
+          console.log(actual.data)
+          return actual.data
+        }
+      }
+
+    }
+    console.log("no se encontro")
+    return null*/
+
+  }
+}
+
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +127,52 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+
+class HashTable {
+  constructor() {
+    this.numBuckets = 35;
+    this.buckets = new Array(this.numBuckets);
+    this.buckets.fill([]);
+
+  }
+  hash(key) {
+    let sum = 0;
+    for (let i = 0; i < key.length; i++) {
+      sum = sum + key.charCodeAt(i);
+    }
+    return sum % this.numBuckets;
+  }
+  set(clave, valor) {
+    if (typeof clave !== "string") throw new TypeError("no es string")
+
+    let posicion = this.hash(clave)
+    this.buckets[posicion].unshift({
+      key: clave,
+      value: valor
+    })
+    
+  }
+  get(clave) {
+    let posicion = this.hash(clave)
+
+    for (let i = 0; i < this.buckets[posicion].length; i++) {
+      if (this.buckets[posicion][i].key === clave) {
+        return this.buckets[posicion][i].value
+      }
+    }
+    return false
+
+  }
+  hasKey(z) {
+    let existe = this.get(z)
+    if (existe) return true
+    else return false
+  }
+
+
+}
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,120 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+class BinarySearchTree {
+  constructor(valor) {
+    this.value = valor;
+    this.right = null;
+    this.left = null;
+  }
+  
+  size() {
+    if (!this.right && !this.left){ 
+      return 1
+    } else if (this.right && this.left) {
+      return 1 + this.right.size() + this.left.size()
+    } else if (this.right && !this.left) {
+      return 1 + this.right.size()
+
+    } else if (!this.right && this.left) {
+      return 1 + this.left.size()
+
+    }
+  }
+  insert(value) {
+    var hoja = new BinarySearchTree(value)
+    
+    if (value > this.value) {
+      if (!this.right) {
+        this.right = hoja
+
+      } else {
+        this.right.insert(value)
+      }
+    } else {
+      if (!this.left) {
+        this.left = hoja
+        
+      } else { 
+        this.left.insert(value)
+      }
+    }
+
+
+  }
+  contains(value) {
+    if (value === this.value) return true
+    if (value > this.value) {
+      if (!this.right) return false
+      else return this.right.contains(value)
+
+    } else {
+      if (!this.left) return false
+      else return this.left.contains(value)
+
+    }
+   
+
+
+  }
+  depthFirstForEach(callBack, order) {
+    if (order === "pre-order") {
+        callBack(this.value);
+        if (this.left) {
+            this.left.depthFirstForEach(callBack, order);
+        }
+
+        if (this.right) {
+            this.right.depthFirstForEach(callBack, order);
+        }
+    } else if (order === "post-order") {
+        if (this.left) {
+            this.left.depthFirstForEach(callBack, order);
+        }
+
+        if (this.right) {
+            this.right.depthFirstForEach(callBack, order);
+        }
+        callBack(this.value);
+    } else {
+        // in-order
+        if (this.left) {
+            this.left.depthFirstForEach(callBack, order);
+        }
+        callBack(this.value);
+
+        if (this.right) {
+            this.right.depthFirstForEach(callBack, order);
+        }
+    }
+}
+breadthFirstForEach(callBack, array) {
+    if (array == null) {
+        var array = [];
+    }
+
+    if (this.left) {
+        array.push(this.left);
+    }
+
+    if (this.right) {
+        array.push(this.right);
+    }
+
+    callBack(this.value);
+
+    array.length > 0 && array.shift().breadthFirstForEach(callBack, array);
+    // if(array.length > 0) array.shift().breadthFirstForEach(callBack, array)
+}
+
+
+
+
+
+
+}
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
